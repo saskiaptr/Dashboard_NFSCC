fc1e1f392db3868e90c2be47e322eef4b2079360
kia08040 <saskiaptr844@gmail.com>
2026-05-28 17:53:38 +0700

    fix options cors

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::options('/{any}', function () {
+    return response('', 204);
+})->where('any', '.*');
